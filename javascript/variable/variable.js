c00e08d7ae6d892a37a83b10645cee9b2d77c746
MD.Owes Quruny Shubho <shubhomedia@gmail.com>
2021-07-31 08:34:41 +0600

    New code added

--- javascript/variable/variable.js
+++ javascript/variable/variable.js
@@ -1,7 +1,10 @@
 // This is variable 
 
-var name = "MD. Owes Shubho";
-var age = 29;
-var height = 5.6;
+var name = "MD. Owes Shubho"; // String Type
+var age = 29; // Number or Numerical Type
+var height = 5.6; 
+
+var payment = true;
+var due = false;
 
 console.log(name);
