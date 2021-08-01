85b865fd251ec3a5f51cce6491eb6a5556af6083
MD.Owes Quruny Shubho <shubhomedia@gmail.com>
2021-08-01 14:46:56 +0600

    New js codeline added

--- javascript/calculation.js
+++ javascript/calculation.js
@@ -3,3 +3,15 @@ var price2 = 200;
 var price3 = 400;
 
 var qualtity = 100;
+
+var sumOfAllPrice = price1 + price2 + price3 ;
+// console.log(sumOfAllPrice); 
+
+var sumOfPrice1AndPrice2 = price1 + price2 ;
+// console.log(sumOfPrice1AndPrice2);
+
+var substract = price3 - price1;
+// console.log(substract);
+
+var multiplication = price1 * qualtity;
+console.log(multiplication);
