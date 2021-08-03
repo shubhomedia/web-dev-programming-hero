95de3e643a63314468ec33e09cc2b494dec7636e
MD.Owes Quruny Shubho <shubhomedia@gmail.com>
2021-08-03 20:47:40 +0600

    New type related code added

--- javascript/variable/parseint.js
+++ javascript/variable/parseint.js
@@ -14,5 +14,13 @@ var perEggPrice = 8.5;
 // var sumOfAll = shopingTotal + parseInt(perEggPrice); 
 var sumOfAll = shopingTotal + parseFloat(perEggPrice); 
 
-console.log(sumOfAll);
+// console.log(sumOfAll); 
 
+var tShirtPrice = 100;
+var tShirtPrice2 = '100';
+var tShirtPrice3 = 100.100;
+var newprice = true;
+console.log(typeof tShirtPrice);
+console.log(typeof tShirtPrice2);
+console.log(typeof tShirtPrice3);
+console.log(typeof newprice);
