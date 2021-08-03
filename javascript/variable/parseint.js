341e02ba4af3d6ea0dea81e8a2cd8852196ad5ff
MD.Owes Quruny Shubho <shubhomedia@gmail.com>
2021-08-03 20:36:57 +0600

    parseint and sting combination code added

--- javascript/variable/parseint.js
+++ javascript/variable/parseint.js
@@ -1,4 +1,10 @@
 var data1 = '100';
 var data2 = '200';
 var totalData= parseInt(data1) + parseInt(data2);
-console.log(totalData);
+// console.log(totalData);
+
+var firstName = 'Owes';
+var lastName = 'Shubho';
+var fullName = firstName + ' ' + lastName;
+console.log(fullName);
+
