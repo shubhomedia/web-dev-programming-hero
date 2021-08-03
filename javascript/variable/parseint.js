60a3ca8a631d35f6de1ed48ab0f4756c70f63b7b
MD.Owes Quruny Shubho <shubhomedia@gmail.com>
2021-08-03 20:41:14 +0600

    Parseint and ParseFloot code added

--- javascript/variable/parseint.js
+++ javascript/variable/parseint.js
@@ -6,5 +6,13 @@ var totalData= parseInt(data1) + parseInt(data2);
 var firstName = 'Owes';
 var lastName = 'Shubho';
 var fullName = firstName + ' ' + lastName;
-console.log(fullName);
+// console.log(fullName); 
+
+var shopingTotal = 100;
+var perEggPrice = 8.5;
+// var sumOfAll = shopingTotal + perEggPrice; 
+// var sumOfAll = shopingTotal + parseInt(perEggPrice); 
+var sumOfAll = shopingTotal + parseFloat(perEggPrice); 
+
+console.log(sumOfAll);
 
