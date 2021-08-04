8284b3191f6fc82550873039da6196803bfd0df5
MD.Owes Quruny Shubho <shubhomedia@gmail.com>
2021-08-04 22:28:04 +0600

    New array code writen

--- javascript/variable/array.js
+++ javascript/variable/array.js
@@ -4,3 +4,10 @@ var fees = [10,20,30,40,50,60,70];
 var counter = friends.length;
 console.log(fees);
 console.log(counter);
+
+var newindex = friends.indexOf('kamal');
+console.log(newindex);
+var index2 = friends[2];
+// console.log(index2);
+friends [3] = ['jabbar'];
+console.log(friends);
