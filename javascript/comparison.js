ebc850dfee3355f64edf332400d2324dce2b5524
MD.Owes Quruny Shubho <shubhomedia@gmail.com>
2021-08-05 07:28:10 +0600

    New comparison code added

---
+++ javascript/comparison.js
@@ -0,0 +1,18 @@
+var x, y, z ;
+x = 10;
+y = 20;
+z = 30;
+
+var eual;
+ x == 10 ;
+console.log(x == 10);
+console.log(x == 20);
+console.log(x === 20);
+console.log(x > 20);
+console.log(x < 20);
+console.log(x != 20);
+console.log(x >= 20);
+console.log(x <= 20);
+console.log(x <= 20);
+console.log(x <10 && y > 10);
+console.log(x <10 || y > 10);
