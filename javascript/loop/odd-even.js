cfe8efc87a0e195fbb3757d3f834647bf6edd2ab
MD.Owes Quruny Shubho <shubhomedia@gmail.com>
2021-08-05 23:19:44 +0600

    New Odd Even code added

--- javascript/loop/odd-even.js
+++ javascript/loop/odd-even.js
@@ -1,13 +1,26 @@
 // Print even Number 
 var number = 1;
 while (number <=10){
-    console.log(number);
+    // console.log(number);
     number = number + 2
 }
 
 // Print even Number 
 var number = 0;
 while (number <=10){
-    console.log(number);
+    // console.log(number);
     number = number + 2
 }
+
+// Odd number 
+var i = 0 ;
+for (i = 1; i <=10 ; i+=2 ){
+    console.log(i);
+}
+
+// Even number 
+var i = 0 ;
+for (i = 0; i <=10 ; i+=2 ){
+    console.log(i);
+}
+
