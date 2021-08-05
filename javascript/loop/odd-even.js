fe157179e446be9aaacdbd65503b6e5cd89adef3
MD.Owes Quruny Shubho <shubhomedia@gmail.com>
2021-08-05 23:01:59 +0600

    New even and odd number added

--- javascript/loop/odd-even.js
+++ javascript/loop/odd-even.js
@@ -1,5 +1,13 @@
+// Print even Number 
 var number = 1;
 while (number <=10){
     console.log(number);
-    number++;
+    number = number + 2
+}
+
+// Print even Number 
+var number = 0;
+while (number <=10){
+    console.log(number);
+    number = number + 2
 }
