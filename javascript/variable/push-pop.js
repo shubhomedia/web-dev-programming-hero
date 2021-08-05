9dfcad74888a50b41b0d3675f6c0085d1bb30929
MD.Owes Quruny Shubho <shubhomedia@gmail.com>
2021-08-05 07:16:34 +0600

    New Push at first position added

--- javascript/variable/push-pop.js
+++ javascript/variable/push-pop.js
@@ -2,9 +2,15 @@ var lastBench = ['kalam', 'jamal','salam'];
 
 // Add new Item to array 
 lastBench.push('shubho');
-console.log(lastBench);
+// console.log(lastBench);
 
 // Remove Item from Last Index 
 var lastItem = lastBench.pop();
-console.log(lastItem);
-console.log(lastBench);
+// console.log(lastItem);
+// console.log(lastBench);
+
+var fruits = ['Banana','Orange','Apple','Mango'];
+fruits.push('Kiwi','Lemon','Pineapple');
+console.log(fruits);
+fruits.splice(0,0,"Dragon");
+console.log(fruits);
