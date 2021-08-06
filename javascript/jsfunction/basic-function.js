effa059e8bb24f6dc408a31a59e5add050dae112
MD.Owes Quruny Shubho <shubhomedia@gmail.com>
2021-08-06 08:55:01 +0600

    Price conter 2 function writen

--- javascript/jsfunction/basic-function.js
+++ javascript/jsfunction/basic-function.js
@@ -12,3 +12,12 @@ function priceCounter(amount){
     console.log(amount);
 }
 priceCounter(100);
+
+// New Function with Parameter 
+function priceCounter2(amount){
+    console.log("You Paid",amount);
+    var price = 10;
+    var amoutprice = amount / price;
+    console.log(amoutprice);
+}
+priceCounter2(100);
