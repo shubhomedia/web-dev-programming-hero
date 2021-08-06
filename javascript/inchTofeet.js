a980bef037fcb32252f124525cfcb0ade18add2c
MD.Owes Quruny Shubho <shubhomedia@gmail.com>
2021-08-06 15:06:24 +0600

    Inch to feet and mile to km conterver

---
+++ javascript/inchTofeet.js
@@ -0,0 +1,13 @@
+
+// Inch to feet conveter 
+function inchTofeet (inch){
+    var feet = inch / 12;
+    return feet;
+}
+
+// Inch to feet conveter 
+function mileToKm (mile){
+    var km = 0.621371 * mile;
+    return feet;
+}
+
