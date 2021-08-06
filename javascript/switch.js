9d820d97e4581074b85fa6b147d49fbfced41d97
MD.Owes Quruny Shubho <shubhomedia@gmail.com>
2021-08-06 12:55:01 +0600

    New switch case code added

---
+++ javascript/switch.js
@@ -0,0 +1,22 @@
+
+var color = 'red';
+
+switch (color){
+    case 'red':
+        console.log("This is red block");
+    break;
+    case 'blue':
+        console.log("This is blue block");
+    break;
+    case 'white':
+        console.log("This is white block");
+    break;
+    case 'green':
+        console.log("This is green block");
+    break;
+    case 'black':
+        console.log("This is black block");
+    break;
+    default:
+        console.log("This is No Colors Block");
+}
