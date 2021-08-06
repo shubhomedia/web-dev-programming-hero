9d69d74ffbad47abb3e9a5edcb0d59b0e56fa1e7
MD.Owes Quruny Shubho <shubhomedia@gmail.com>
2021-08-06 09:28:51 +0600

    New multiparameter function added

---
+++ javascript/jsfunction/multi-parameter-function.js
@@ -0,0 +1,19 @@
+
+// Sum of Two Number with multi parameter 
+function sum (number1, number2){
+    var sum = number1 + number2;
+    console.log(sum);
+}
+
+var number1 = 20, number2 = 30;
+sum(number1,number2);
+
+// Multiplication of Two Number with multi parameter 
+function multi (number1, number2){
+    var result = number1 * number2;
+    console.log(result);
+}
+
+var number1 = 20, number2 = 30;
+multi(number1,number2);
+
