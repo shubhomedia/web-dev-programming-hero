5629f2a5265e35e9852d15ada86227343382384d
MD.Owes Quruny Shubho <shubhomedia@gmail.com>
2021-08-07 08:20:10 +0600

    Factorial Code added

---
+++ javascript/factorial.js
@@ -0,0 +1,12 @@
+
+
+function factorial(number){
+    var factorial = 1
+    for (var i = 1; i <= number; i++){
+        factorial = factorial * i;
+    }
+    console.log(factorial);
+}
+
+factorial(6);
+
