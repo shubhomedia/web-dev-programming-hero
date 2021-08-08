19929ea520e67ce0c6b24fd5558aae0a136f1256
MD.Owes Quruny Shubho <shubhomedia@gmail.com>
2021-08-08 22:30:17 +0600

    New js code added

--- javascript/js-practice/fibonnaci.js
+++ javascript/js-practice/fibonnaci.js
@@ -1,4 +1,10 @@
 function fibonacciSeries(num){
+    if (typeof num!= 'number'){
+        return 'Please Give a Number';
+    }
+    if (num < 2){
+        return 'Please enter a postive number greater than 1'
+    }
     const fibo = [0,1];
     for (let i = 2; i <= num; i++){
         fibo[i]=fibo[i-1]+fibo[i-2];
@@ -6,5 +12,5 @@ function fibonacciSeries(num){
     return fibo;
 }
 
-const fiboseries = fibonacciSeries(13);
+const fiboseries = fibonacciSeries(10);
 console.log(fiboseries);
