514ca14110a52bcef2fa9f66c5736c589c96f0ac
MD.Owes Quruny Shubho <shubhomedia@gmail.com>
2021-08-08 07:29:02 +0600

    New max code apply

--- javascript/math-js/all-practice.js
+++ javascript/math-js/all-practice.js
@@ -5,3 +5,7 @@ let three = 11;
 let maxNumber  = Math.max(one,two,three);
 let minNumber  = Math.max(one,two,three);
 console.log(maxNumber);
+
+let newArray = [10,20,30,40,50,60,70,8,0];
+let max = Math.max.apply(null,newArray);
+console.log(max);
