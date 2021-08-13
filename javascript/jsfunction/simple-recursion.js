e19329e657f4016626d76ed175a340c825eabe27
MD.Owes Quruny Shubho <shubhomedia@gmail.com>
2021-08-13 22:51:46 +0600

    Facetorila code recurision added

--- javascript/jsfunction/simple-recursion.js
+++ javascript/jsfunction/simple-recursion.js
@@ -5,4 +5,16 @@ function sum(i){
     return i + sum(i-1);
 }
 
-console.log(sum(6));
+// console.log(sum(6));
+
+// Factorial Recursion 
+
+function factorial (i){
+    if (i == 1){
+        return 1
+    }
+    return i * factorial(i-1);
+}
+
+console.log(factorial(6));
+
