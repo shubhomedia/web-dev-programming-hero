dfbe2b25222b8c8503332a72a7377a6eb0eb27d5
MD.Owes Quruny Shubho <shubhomedia@gmail.com>
2021-08-18 15:09:53 +0600

    Added new ready project

---
+++ practice-projects/calculator/js/calculator.js
@@ -0,0 +1,64 @@
+const calculator =document.querySelector('.calculator');
+const keys = calculator.querySelector('.calculator__keys');
+
+keys.addEventListener('click', e => {
+    if (e.target.matches('button')) {
+        const key = e.target;
+        const action = key.dataset.action;
+        if (!action) {
+            console.log('number key!')
+          }
+        if (
+            action === 'add' ||
+            action === 'subtract' ||
+            action === 'multiply' ||
+            action === 'divide'
+          ) {
+            console.log('operator key!')
+          }
+          if (action === 'decimal') {
+            console.log('decimal key!')
+          }
+          
+          if (action === 'clear') {
+            console.log('clear key!')
+          }
+          
+          if (action === 'calculate') {
+            console.log('equal key!')
+          }
+    }
+   });
+
+   const display = document.querySelector('.calculator__display')
+
+   keys.addEventListener('click', e => {
+     if (e.target.matches('button')) {
+       const key = e.target
+       const action = key.dataset.action
+       const keyContent = key.textContent
+       const displayedNum = display.textContent
+       
+       if (!action) {
+        if (displayedNum === '0') {
+          display.textContent = keyContent
+        } else {
+          display.textContent = displayedNum + keyContent
+        }
+      }
+      if (action === 'decimal') {
+        display.textContent = displayedNum + '.'
+      }
+      if (
+        action === 'add' ||
+        action === 'subtract' ||
+        action === 'multiply' ||
+        action === 'divide'
+      ) {
+        key.classList.add('is-depressed')
+      }
+     }
+     Array.from(key.parentNode.children)
+      .forEach(k => k.classList.remove('is-depressed'))
+   })
+   
