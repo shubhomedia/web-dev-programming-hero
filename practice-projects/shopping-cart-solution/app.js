8f840de0439127bfc16f2b0e3bba289fd65e6cc5
MD.Owes Quruny Shubho <shubhomedia@gmail.com>
2021-08-18 21:10:36 +0600

    new function added

--- practice-projects/shopping-cart-solution/app.js
+++ practice-projects/shopping-cart-solution/app.js
@@ -1,23 +1,38 @@
-function updateCaseNumber(isIncreasing){
-    const caseInput = document.getElementById('case-number');
-    let caseNumber = caseInput.value;
-
-    if(isIncreasing){
-        caseNumber = parseInt(caseNumber) + 1;
+function updateProductNumber(product, price, isIncreasing) {
+    const productInput = document.getElementById(product + '-number');
+    let productNumber = productInput.value;
+    if (isIncreasing == true) {
+        productNumber = parseInt(productNumber) + 1;
     }
-    else if(caseNumber > 0){
-        caseNumber = parseInt(caseNumber) - 1;
+    else if (productNumber > 0) {
+        productNumber = parseInt(productNumber) - 1;
     }
-    caseInput.value = caseNumber;
-
-    const caseTotal = document.getElementById('case-total');
-    caseTotal.innerText = caseNumber * 59;
+    productInput.value = productNumber;
+    // update total
+    const productTotal = document.getElementById(product + '-total');
+    productTotal.innerText = productNumber * price;
+    // calculate total
+    calculateTotal();
 }
 
-document.getElementById('case-plus').addEventListener('click',function(){
-    updateCaseNumber(true);
-});
+function getInputValue(product) {
+    const productInput = document.getElementById(product + '-number');
+    const productNumber = parseInt(productInput.value);
+    return productNumber;
+}
 
-document.getElementById('case-minus').addEventListener('click',function(){
-    updateCaseNumber(false);
+// phone increase decrease events
+document.getElementById('phone-plus').addEventListener('click', function () {
+    updateProductNumber('phone', 1219, true);
 });
+document.getElementById('phone-minus').addEventListener('click', function () {
+    updateProductNumber('phone', 1219, false);
+})
+
+// handle case increase decrease events
+document.getElementById('case-plus').addEventListener('click', function () {
+    updateProductNumber('case', 59, true);
+});
+document.getElementById('case-minus').addEventListener('click', function () {
+    updateProductNumber('case', 59, false);
+})
