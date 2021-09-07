4b038a37bc463ce5965ee3d7660c4be239ef433d
MD.Owes Quruny Shubho <shubhomedia@gmail.com>
2021-09-07 15:31:23 +0600

    New code added

---
+++ javascript/browser-milestone/js-timeout.js
@@ -0,0 +1,7 @@
+console.log("This is line 1");
+console.log("This is line 2");
+setTimeout(function(){
+    console.log("Time out Block")
+},5000);
+console.log("This is line 3");
+console.log("This is line 4");
