499731de99996232fec00f54eb5180d6a268eec8
MD.Owes Quruny Shubho <shubhomedia@gmail.com>
2021-09-09 10:07:51 +0600

    New js basic code recap

---
+++ javascript/all-recap/basic.js
@@ -0,0 +1,14 @@
+// Variable 
+const userName = "shubhobd";
+let salary = 20000;
+
+// Condition 
+if (salary < 30000){
+    console.log("Salary Less then 30000");
+}
+else if (salary > 20000) {
+    console.log("Salary greater then 20000");
+}
+else {
+    console.log("Salary between 20000 and 30000");
+}
