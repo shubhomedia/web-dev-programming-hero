dd68f540af6f2a6d2dc307e7e02a78241cd38e9b
MD.Owes Quruny Shubho <shubhomedia@gmail.com>
2021-09-15 16:05:44 +0600

    Kanye quote project

---
+++ javascript/API/kanye-api/kenye-api.js
@@ -0,0 +1,10 @@
+const getdata = () => {
+    fetch('https://api.kanye.rest/')
+    .then(res => res.json())
+    .then(data => displayQuote(data));
+}
+
+const displayQuote = quote => {
+    const quoteElement = document.getElementById('quote');
+    quoteElement.innerText = quote.quote;
+}
