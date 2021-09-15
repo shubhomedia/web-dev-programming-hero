1e8a5cf3ebea5ba240c035d2760226da38059dc8
MD.Owes Quruny Shubho <shubhomedia@gmail.com>
2021-09-15 12:03:33 +0600

    New code added

--- javascript/API/basic-api.js
+++ javascript/API/basic-api.js
@@ -1,7 +1,13 @@
-
-
   function loadAllData (){
     fetch('https://jsonplaceholder.typicode.com/users')
     .then(response => response.json())
-    .then(json => console.log(json))
+    .then(data => displayAll(data))
+  }
+
+  function displayAll(data){
+    // console.log(data);
+
+    for (const users of data){
+        console.log(users.name);
+    }
   }
