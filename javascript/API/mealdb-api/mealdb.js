f10e91a3a03f787b70a8143973051011943295c6
MD.Owes Quruny Shubho <shubhomedia@gmail.com>
2021-09-17 21:07:16 +0600

    new project meal db added

---
+++ javascript/API/mealdb-api/mealdb.js
@@ -0,0 +1,39 @@
+const searchFood = () => {
+    const searchField = document.getElementById('search-field');
+    const searchText = searchField.value;
+    searchField.value = '';
+    hideBgImage()
+    const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${searchText}`;
+    
+    fetch(url)
+    .then(res => res.json())
+    .then(data =>displaySearchResult(data.meals));
+}
+
+const displaySearchResult = meals => {
+    const searchResult = document.getElementById('search-result');
+    meals.forEach(meal => {
+        console.log(meal);
+        const div = document.createElement('div');
+        div.classList.add('col');
+        div.innerHTML = `
+        <div class="card h-100">
+            <img src="${meal.strMealThumb}" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">${meal.strMeal}</h5>
+                <p class="card-text">${meal.strInstructions.slice(0,500)}</p>
+            </div>
+        </div>
+        `;
+        searchResult.appendChild(div);
+    });
+}
+
+function hideBgImage() {
+    var x = document.getElementById("myDIV");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
