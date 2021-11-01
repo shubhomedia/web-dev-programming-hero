40bf0acb1c937822d57da2c6e30a9d728755cbd6
MD.Owes Quruny Shubho <shubhomedia@gmail.com>
2021-11-01 08:29:07 +0600

    new coded added

--- javascript/dbcode/app.js
+++ javascript/dbcode/app.js
@@ -2,10 +2,28 @@ const db = {};
 
 const addToDb = item => {
     const db =getDb();
-    db[item]= 1;
+    if (item in db){
+        db[item] =db[item] + 1;
+    }
+    else {
+        db[item] =1;
+    }
+    saveToDb(db);
 }
 
+const removeFromDb = item =>{
+    const db = getDb();
+    delete db[item];
+    saveToDb(db);
+}
+
+const saveToDb =db => {
+    const dbJSON = JSON.stringify(db)
+    localStorage.setItem('shopping-cart',dbJSON);
+}
 
 const getDb = () =>{
-    return db;
+    let savedDb = localStorage.getItem('shopping-cart');
+    savedDb = JSON.parse(savedDb);
+    return savedDb;
 }
