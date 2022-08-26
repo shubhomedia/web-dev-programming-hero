f73ca9fbb4f14b4b0a62e96bb0da22a92cba6ebf
MD.Owes Quruny Shubho <shubhomedia@gmail.com>
2022-08-26 11:19:00 +0600

    Props send and received

--- Milestone8/SimpleReact/module8-first-react/src/App.js
+++ Milestone8/SimpleReact/module8-first-react/src/App.js
@@ -8,16 +8,31 @@ const nameValue = {
 function App() {
   return (
     <div className="App">
+      <Person Name="Jamal" Title="Student"></Person>
       <Person></Person>
+      <Person Name="Shubho" Title="Web Developer"></Person>
+      <ProgrammingLang name="Go" year="2013"></ProgrammingLang>
+      <ProgrammingLang name="Java" year="1993"></ProgrammingLang>
+      <ProgrammingLang name="Python" year="1992"></ProgrammingLang>
     </div>
   );
 }
 
-function Person() {
+function Person(props) {
+  console.log(props);
   return (
     <div className="personCard">
-      <h1>Name: {nameValue.Name}</h1>
-      <h2>Title: {nameValue.Title}</h2>
+      <h1>Name: {props.Name}</h1>
+      <h2>Title: {props.Title}</h2>
+    </div>
+  );
+}
+
+function ProgrammingLang(props) {
+  return (
+    <div className="programmingLang">
+      <h1>Programming Language Name: {props.name}</h1>
+      <h2>Programming Language Year: {props.year}</h2>
     </div>
   );
 }
