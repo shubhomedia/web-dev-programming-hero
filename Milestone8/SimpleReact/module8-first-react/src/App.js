31843369a7bbf712a1b5f8439ba7f015e8e71e36
MD.Owes Quruny Shubho <shubhomedia@gmail.com>
2022-08-28 07:32:18 +0600

    Update App. JS
    New UseStage and UseEffect Functions
    Working with users data and show data

--- Milestone8/SimpleReact/module8-first-react/src/App.js
+++ Milestone8/SimpleReact/module8-first-react/src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 // const HeroDb = [
 //   { name: 'Amir Khan', movie: 'Tare Zamen Par' },
@@ -12,24 +12,51 @@ import React, { useState } from 'react';
 function App() {
   return (
     <div className="App">
-      <Counter></Counter>
+      {/* <Counter></Counter> */}
+      <ExternalUsers></ExternalUsers>
     </div>
   );
 }
 
-function Counter() {
-  const [count, setCount] = useState(0);
-  const handleincrease = () => setCount(count + 1);
-  const handledecrease = () => setCount(count - 1);
-
+function ExternalUsers() {
+  const [users, setUsers] = useState([]);
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(res => res.json())
+      .then(data => setUsers(data))
+  }, [])
   return (
     <div>
-      <h1>Count: {count}</h1>
-      <button onClick={handleincrease}>Increase</button>
-      <button onClick={handledecrease}>Decrease</button>
+      <h3>External Users: </h3>
+      {
+        users.map(user => <User name={user.name} email={user.email}></User>)
+      }
     </div>
   )
 }
+
+function User(props) {
+  return (
+    <div>
+      <h2>Name: {props.name}</h2>
+      <p>Email: {props.email} </p>
+    </div>
+  )
+}
+
+// function Counter() {
+//   const [count, setCount] = useState(0);
+//   const handleincrease = () => setCount(count + 1);
+//   const handledecrease = () => setCount(count - 1);
+
+//   return (
+//     <div>
+//       <h1>Count: {count}</h1>
+//       <button onClick={handleincrease}>Increase</button>
+//       <button onClick={handledecrease}>Decrease</button>
+//     </div>
+//   )
+// }
 
 // function ActorName(props) {
 //   return (
