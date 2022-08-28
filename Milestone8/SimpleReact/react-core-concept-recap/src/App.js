cdbcfe1b2b430320f9ed30f71e074b46f3eaf9e1
MD.Owes Quruny Shubho <shubhomedia@gmail.com>
2022-08-28 21:22:52 +0600

    Update New Counter Code
    Load New Counter with React

--- Milestone8/SimpleReact/react-core-concept-recap/src/App.js
+++ Milestone8/SimpleReact/react-core-concept-recap/src/App.js
@@ -1,25 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useEffect, useState } from 'react';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Counter></Counter>
     </div>
   );
+}
+
+
+
+function Counter() {
+  const [count, setcount] = useState(0);
+  const handleIncrease = () => setcount(count + 1);
+  const handleDecrease = () => setcount(count - 1);
+  return (
+    <div>
+      <h2>Count: {count}</h2>
+      <button onClick={handleIncrease}>Increase: </button>
+      <button onClick={handleDecrease}>Decrease: </button>
+    </div>
+  )
 }
 
 export default App;
