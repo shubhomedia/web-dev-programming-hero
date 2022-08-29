b83fcf02ddd76b51d55164ca3ff3ac6c2f2dba70
MD.Owes Quruny Shubho <shubhomedia@gmail.com>
2022-08-29 07:35:59 +0600

    new users info load from jsonplaceholder
    new user info load components

--- Milestone8/SimpleReact/react-core-concept-recap/src/App.js
+++ Milestone8/SimpleReact/react-core-concept-recap/src/App.js
@@ -5,30 +5,57 @@ import React, { useEffect, useState } from 'react';
 function App() {
   return (
     <div className="App">
-      <MyComponent job={'WebDev'} title={'WPDev'}></MyComponent>
+      {/* <MyComponent job={'WebDev'} title={'WPDev'}></MyComponent>
       <MyComponent job={'DBDev'} title={'Laravel'}></MyComponent>
-      <MyComponent job={'ReactJS'} title={'FrontEnd'}></MyComponent>
+      <MyComponent job={'ReactJS'} title={'FrontEnd'}></MyComponent> */}
+      <LoadUsers></LoadUsers>
+    </div>
+  );
+}
+function LoadUsers(props) {
+  const [users, setUsers] = useState([])
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(res => res.json())
+      .then(data => setUsers(data))
+
+  }, [])
+  return (
+    <div>
+      {
+        users.map(user => <User name={user.name} username={user.username} email={user.email}></User>)
+      }
     </div>
   );
 }
 
-function MyComponent(props) {
-  const [points, setPoints] = useState(1)
-
-  const handleclick = () => {
-    const newPoints = points * 2;
-    setPoints(newPoints);
-  }
+function User(props) {
   return (
     <div className='myComponent'>
-      <h1>Hello From Shubho, This is My Component</h1>
-      <h2>Job: {props.job}</h2>
-      <h2>Title: {props.title}</h2>
-      <h3>Points : {points} </h3>
-      <button onClick={handleclick}>Add Points</button>
+      <h2>Name: {props.name} </h2>
+      <h3>User Name:{props.username} </h3>
+      <h4>Email:{props.email} </h4>
     </div>
-  );
+  )
 }
+
+// function MyComponent(props) {
+//   const [points, setPoints] = useState(1)
+
+//   const handleclick = () => {
+//     const newPoints = points * 2;
+//     setPoints(newPoints);
+//   }
+//   return (
+//     <div className='myComponent'>
+//       <h1>Hello From Shubho, This is My Component</h1>
+//       <h2>Job: {props.job}</h2>
+//       <h2>Title: {props.title}</h2>
+//       <h3>Points : {points} </h3>
+//       <button onClick={handleclick}>Add Points</button>
+//     </div>
+//   );
+// }
 // function LoadComments() {
 //   const [comments, setcomments] = useState([]);
 //   useEffect(() => {
