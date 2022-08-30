e1dff748b4545e33e7aa5d29cd75c4020bab0513
MD.Owes Quruny Shubho <shubhomedia@gmail.com>
2022-08-30 07:39:35 +0600

    New file added

--- Milestone8/SimpleReact/react-rest-countries/src/App.js
+++ Milestone8/SimpleReact/react-rest-countries/src/App.js
@@ -1,11 +1,12 @@
-import logo from './logo.svg';
+
 import './App.css';
-// import React, { useEffect, useState } from 'react';
+import Countries from './Components/Countries/Countries';
+
 
 function App() {
   return (
     <div className="App">
-
+      <Countries></Countries>
     </div>
   );
 }
