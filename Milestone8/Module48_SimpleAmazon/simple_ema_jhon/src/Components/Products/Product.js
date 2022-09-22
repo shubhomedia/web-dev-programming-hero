4ddfcb8ac0eaff287ba0479f388671ff7c14ce10
MD.Owes Quruny Shubho <shubhomedia@gmail.com>
2022-09-22 08:18:35 +0600

    new code added

--- Milestone8/Module48_SimpleAmazon/simple_ema_jhon/src/Components/Products/Product.js
+++ Milestone8/Module48_SimpleAmazon/simple_ema_jhon/src/Components/Products/Product.js
@@ -3,9 +3,20 @@ import './Product.css';
 
 const Product = (props) => {
     console.log(props.product)
+    const { name, img, seller, price, stock } = props.product;
     return (
-        <div>
-            <h4>{props.product.name}</h4>
+        <div className="product">
+            <div className='product-img'>
+                <img src={img} alt="" />
+            </div>
+            <div>
+                <h4 className="product-name">{props.product.name}</h4>
+
+                <p><small>By:{seller}</small></p>
+                <p>Price:{price}</p>
+                <p><small>Only {stock} Left in Stock- Order Soon</small></p>
+            </div>
+
         </div>
     );
 };
