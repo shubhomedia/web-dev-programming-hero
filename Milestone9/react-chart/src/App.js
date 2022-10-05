b71e724572c6ecc4e420ab82e5c426518e72e525
MD.Owes Quruny Shubho <shubhomedia@gmail.com>
2022-10-05 23:17:01 +0600

    new projects added

---
+++ Milestone9/react-chart/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import MyCharts from './Components/MyCharts/MyCharts';
+
+function App() {
+  return (
+    <div className="App">
+      <h2>Hello</h2>
+      <MyCharts></MyCharts>
+    </div>
+  );
+}
+
+export default App;
