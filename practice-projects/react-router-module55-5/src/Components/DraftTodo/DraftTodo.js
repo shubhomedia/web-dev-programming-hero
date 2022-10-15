cfcda5752fbe8f5cef4f7ec84dbc85cd712b0744
MD.Owes Quruny Shubho <shubhomedia@gmail.com>
2022-10-15 21:12:38 +0600

    New Project Start

---
+++ practice-projects/react-router-module55-5/src/Components/DraftTodo/DraftTodo.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const DraftTodo = () => {
+    return (
+        <div>
+            <h2>Just Simple Draft Todo</h2>
+        </div>
+    );
+};
+
+export default DraftTodo;
