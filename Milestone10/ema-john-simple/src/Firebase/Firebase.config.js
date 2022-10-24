39ae5cfa2b181520f1c303e4893762fa1f0035d4
MD.Owes Quruny Shubho <shubhomedia@gmail.com>
2022-10-24 08:52:43 +0600

    Firebase New Code Added

---
+++ Milestone10/ema-john-simple/src/Firebase/Firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAWKMZvDrX7cMUis8B6NWqMCihjVBKN9G0",
+    authDomain: "ema-john-simple-82f2e.firebaseapp.com",
+    projectId: "ema-john-simple-82f2e",
+    storageBucket: "ema-john-simple-82f2e.appspot.com",
+    messagingSenderId: "687769318736",
+    appId: "1:687769318736:web:5896c8de777a1d7b0e6855"
+};
+
+export default firebaseConfig;
