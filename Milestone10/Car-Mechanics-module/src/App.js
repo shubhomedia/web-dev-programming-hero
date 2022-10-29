7f61849907afbce7fe7f16ffa763460bef3d3d18
MD.Owes Quruny Shubho <shubhomedia@gmail.com>
2022-10-29 06:39:53 +0600

    Firebase added

--- Milestone10/Car-Mechanics-module/src/App.js
+++ Milestone10/Car-Mechanics-module/src/App.js
@@ -3,7 +3,7 @@ import Home from './Pages/Home/Home/Home';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import NotFound from './Pages/NotFound/NotFound';
 import Booking from './Pages/Booking/Booking/Booking';
-import Login from './Pages/Login/Firebase/Login/Login';
+import Login from './Pages/Login/Login/Login';
 import Header from './Pages/Shared/Header/Header';
 
 function App() {
