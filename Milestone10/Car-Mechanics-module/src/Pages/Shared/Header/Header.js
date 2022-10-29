72cf0f0de5f16b745f4750759fc152e8a1f94f8b
MD.Owes Quruny Shubho <shubhomedia@gmail.com>
2022-10-29 19:28:58 +0600

    New Code Added

--- Milestone10/Car-Mechanics-module/src/Pages/Shared/Header/Header.js
+++ Milestone10/Car-Mechanics-module/src/Pages/Shared/Header/Header.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Button, Container, Nav, Navbar } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import useAuth from '../../../hooks/useAuth';
+import { HashLink } from 'react-router-hash-link';
 
 const Header = () => {
     const { user, logOut } = useAuth();
@@ -11,13 +12,13 @@ const Header = () => {
                 <Container>
                     <Navbar.Brand href="#home">Navbar</Navbar.Brand>
                     <Nav className="me-auto">
-                        <Nav.Link as={Link} to="/home#home">Home</Nav.Link>
-                        <Nav.Link as={Link} to="/home#services">Service</Nav.Link>
+                        <Nav.Link as={HashLink} to="/home#home">Home</Nav.Link>
+                        <Nav.Link as={HashLink} to="/home#services">Service</Nav.Link>
 
                         {user?.email ?
                             <Button onClick={logOut} variant="light">Logout
                             </Button> :
-                            <Nav.Link as={Link} to="/login">Login</Nav.Link>
+                            <Nav.Link as={HashLink} to="/login">Login</Nav.Link>
                         }
 
                     </Nav>
