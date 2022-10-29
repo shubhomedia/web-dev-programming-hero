c48ce0bd15a740784155a4fa0ba13e43b3c27858
MD.Owes Quruny Shubho <shubhomedia@gmail.com>
2022-10-29 07:12:17 +0600

    New Auth Provider Added

---
+++ Milestone10/Car-Mechanics-module/src/hooks/useFirebase.js
@@ -0,0 +1,41 @@
+import { getAuth, signInWithPopup, GoogleAuthProvider, signOut, onAuthStateChanged } from "firebase/auth";
+import { useEffect } from "react";
+import { useState } from "react";
+
+const useFirebase = () => {
+    const [users, setUser] = useState;
+
+    const auth = getAuth();
+    const signInUsingGoogle = () => {
+        const googleProvider = new GoogleAuthProvider();
+        signInWithPopup(auth, googleProvider)
+            .then(result => {
+                setUser(result.user);
+            });
+    }
+    useEffect(() => {
+        const unsubscibed = onAuthStateChanged(auth, user => {
+            if (user) {
+                setUser(user);
+            }
+            else {
+                setUser({})
+            }
+        });
+        return () => unsubscibed;
+    }, [])
+
+    const logOut = () => {
+        signOut(auth)
+            .then(() => {
+
+            })
+    }
+    return {
+        users,
+        signInUsingGoogle,
+        logOut
+    }
+}
+
+export default useFirebase;
