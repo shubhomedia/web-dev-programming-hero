f6ba01e984fd321017727a4b5111c328eb00f8b3
MD.Owes Quruny Shubho <shubhomedia@gmail.com>
2022-11-04 13:48:39 +0600

    Data Load From MongoDB

--- Milestone11/node-mongo-crud-server/index.js
+++ Milestone11/node-mongo-crud-server/index.js
@@ -20,7 +20,12 @@ async function run() {
         const database = client.db("foodMaster");
         const usersCollection = database.collection("user");
         // create a document to insert
-
+        // Get API
+        app.get('/users', async (req, res) => {
+            const cursor = usersCollection.find({});
+            const users = await cursor.toArray();
+            res.send(users);
+        })
         // POST API
         app.post('/users', async (req, res) => {
             const newUser = req.body;
--- Milestone11/node-mongo-crud-starter/src/components/Users/Users.js
+++ Milestone11/node-mongo-crud-starter/src/components/Users/Users.js
@@ -1,9 +1,22 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const Users = () => {
+
+    const [users, setUsers] = useState([]);
+
+    useEffect(() => {
+        fetch('http://localhost:5000/users')
+            .then(res => res.json())
+            .then(data => setUsers(data))
+    }, [])
+
     return (
         <div>
-            <h2>This is Users</h2>
+            <h2>Users:  {users.length}</h2>
+            <ul>
+                {users.map(user => <li key={users._id}>{user.name} :: {user.email}</li>)
+                }
+            </ul>
         </div>
     );
 };
