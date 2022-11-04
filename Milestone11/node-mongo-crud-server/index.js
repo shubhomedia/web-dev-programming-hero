2ac0b026296d9fd8d9ab8b77d4d9de2cd6713a98
MD.Owes Quruny Shubho <shubhomedia@gmail.com>
2022-11-04 07:36:01 +0600

    Node Server Code Update

--- Milestone11/node-mongo-crud-server/index.js
+++ Milestone11/node-mongo-crud-server/index.js
@@ -1,10 +1,20 @@
 const express = require('express');
 const app = express();
 const port = process.eventNames.PORT || 5000;
-
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
 // User : mydbuser1
 // Pass : W6nd6gwVahaYvzeP
+
+
+const uri = "mongodb+srv://mydbuser1:<password>@cluster0.cibjvck.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+    const collection = client.db("test").collection("devices");
+    // perform actions on the collection object
+    client.close();
+});
+
 app.get('/', (req, res) => {
     res.send('Running My Server');
 })
