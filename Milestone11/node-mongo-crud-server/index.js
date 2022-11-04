21be4736269a96b85d3b3f7a1ddf8d82d79e9caf
MD.Owes Quruny Shubho <shubhomedia@gmail.com>
2022-11-04 09:09:06 +0600

    New Data Added Code Deploy

--- Milestone11/node-mongo-crud-server/index.js
+++ Milestone11/node-mongo-crud-server/index.js
@@ -2,28 +2,36 @@ const express = require('express');
 const app = express();
 const port = process.eventNames.PORT || 5000;
 const { MongoClient, ServerApiVersion } = require('mongodb');
+const cors = require('cors');
 
 // User : mydbuser1
 // Pass : W6nd6gwVahaYvzeP
 
+//Middleware
+app.use(cors());
+app.use(express.json());
 
 const uri = "mongodb+srv://mydbuser1:W6nd6gwVahaYvzeP@cluster0.cibjvck.mongodb.net/?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 async function run() {
     try {
+        await client.connect();
         const database = client.db("foodMaster");
         const usersCollection = database.collection("user");
         // create a document to insert
 
         // POST API
-        app.post('/users', (req, res) => {
-            console.log('Hitting The Post');
-            res.send('Hit the post');
+        app.post('/users', async (req, res) => {
+            const newUser = req.body;
+            const result = await usersCollection.insertOne(newUser);
+            console.log('Hitting The Post', req.body);
+            console.log('Added New User', result);
+            res.json(result);
         })
 
     } finally {
-        await client.close();
+        // await client.close();
     }
 }
 run().catch(console.dir);
