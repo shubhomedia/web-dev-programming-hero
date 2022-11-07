0c41ca352f2c088c10e693dfde72fc8e4d0dcc9a
MD.Owes Quruny Shubho <shubhomedia@gmail.com>
2022-11-07 20:19:28 +0600

    Update Manage Service

---
+++ Milestone11/car-mechanics/src/Pages/Home/ManageServices/ManageServices.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import { useEffect } from 'react';
+import { useState } from 'react';
+
+const ManageServices = () => {
+    const [services, setServices] = useState([])
+
+    useEffect(() => {
+        fetch('http://localhost:5000/services')
+            .then(res => res.json())
+            .then(data => setServices(data))
+    }, []);
+
+    const handleDelete = id => {
+        const url = `http://localhost:5000/services/${id}`;
+        fetch(url, {
+            method: 'DELETE'
+        })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data);
+                if (data.detetedCount) {
+                    alert('Deleted');
+                    const remaining = services.filter(service => service._id !== id);
+                    setServices(remaining);
+                }
+
+            })
+    }
+    return (
+        <div>
+            <h2>This is Manage Service</h2>
+            {
+                services.map(service => <div key={service._id}>
+                    <h3>{service.name}</h3>
+                    <button onClick={() => handleDelete(service._id)}>Delete</button>
+                </div>)
+            }
+        </div>
+    );
+};
+
+export default ManageServices;
