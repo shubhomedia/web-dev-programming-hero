1567a51f11056a46dcef255ff5f78843912b0fb4
MD.Owes Quruny Shubho <shubhomedia@gmail.com>
2022-11-07 20:18:06 +0600

    Update Service Update

--- Milestone11/car-mechanics/src/Pages/AddService/AddService.js
+++ Milestone11/car-mechanics/src/Pages/AddService/AddService.js
@@ -1,9 +1,30 @@
 import React from 'react';
-
+import axios from 'axios';
+import { useForm } from "react-hook-form";
+import './AddService.css';
 const AddService = () => {
+    const { register, handleSubmit, reset } = useForm();
+    const onSubmit = data => {
+        console.log(data);
+        axios.post('http://localhost:5000/services', data)
+            .then(res => {
+                if (res.data.insertedId) {
+                    alert('Data Added Successfully');
+                    reset();
+                }
+            })
+    }
+
     return (
-        <div>
+        <div className='add-service'>
             <h2>This is Add Service</h2>
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <input {...register("name", { required: true, maxLength: 20 })} placeholder="Name" />
+                <input {...register("description")} placeholder="Description" />
+                <input type="number" {...register("price")} placeholder="Price" />
+                <input {...register("img")} placeholder="Image" />
+                <input type="submit" />
+            </form>
         </div>
     );
 };
