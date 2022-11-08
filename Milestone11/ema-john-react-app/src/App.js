e2015c615cc42a078547efd200c13022a5fe68a3
MD.Owes Quruny Shubho <shubhomedia@gmail.com>
2022-11-08 11:45:42 +0600

    Update App.js

--- Milestone11/ema-john-react-app/src/App.js
+++ Milestone11/ema-john-react-app/src/App.js
@@ -8,6 +8,7 @@ import OrderReview from './components/OrderReview/OrderReview';
 import PlaceOrder from './components/PlaceOrder/PlaceOrder';
 import PrivateRoute from './components/PrivateRoute/PrivateRoute';
 import Register from './components/Register/Register';
+import Shipping from './components/Shipping/Shipping';
 import Shop from './components/Shop/Shop';
 import AuthProvider from './context/AuthProvider';
 
@@ -30,6 +31,9 @@ function App() {
             <Route path="/inventory">
               <Inventory></Inventory>
             </Route>
+            <PrivateRoute path="/shipping">
+              <Shipping></Shipping>
+            </PrivateRoute>
             <PrivateRoute path="/placeorder">
               <PlaceOrder></PlaceOrder>
             </PrivateRoute>
