14f5232d729cee400c7228572e341eadd1e1eaa1
MD.Owes Quruny Shubho <shubhomedia@gmail.com>
2022-11-08 09:34:49 +0600

    New Project Modified

---
+++ Milestone11/ena-john-node-mongo-server/index.js
@@ -0,0 +1,50 @@
+const express = require('express');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const cors = require('cors');
+const app = express();
+require('dotenv').config();
+const port = process.env.PORT || 5000;
+
+//MiddleWare
+app.use(cors());
+app.use(express.json());
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@emajohn.3qnjdkh.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        await client.connect();
+        console.log('Database Connent Succeessfully');
+
+
+        const database = client.db('online_shop');
+        const productCollection = database.collection('products');
+
+        //GET Product API
+        app.get('/products', async (req, res) => {
+            const cursor = productCollection.find({});
+            const products = await cursor.toArray();
+            const count = await cursor.count();
+
+            res.send({
+                count,
+                products
+            })
+        });
+    }
+    finally {
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
+app.get('/', (req, res) => {
+    res.send('Ema Jon Server Is Running ');
+})
+
+
+app.listen(port, () => {
+    console.log('Server Running At Port:', port);
+})
