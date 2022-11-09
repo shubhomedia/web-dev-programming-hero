1d4d466b307aab85ee7385365c14f6065913e7c8
MD.Owes Quruny Shubho <shubhomedia@gmail.com>
2022-11-09 07:30:52 +0600

    Update Index js File

--- Milestone11/understand-node-express-mongo/index.js
+++ Milestone11/understand-node-express-mongo/index.js
@@ -1,1 +1,11 @@
-console.log('Node JS Running');
+const express = require('express')
+const app = express()
+const port = 7000
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
