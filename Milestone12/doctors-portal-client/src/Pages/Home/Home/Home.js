fb63115a984e78ea9931786ce0f8f5ff1bae3faf
MD.Owes Quruny Shubho <shubhomedia@gmail.com>
2022-11-12 14:04:17 +0600

    Update Home Component

--- Milestone12/doctors-portal-client/src/Pages/Home/Home/Home.js
+++ Milestone12/doctors-portal-client/src/Pages/Home/Home/Home.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import Navigation from '../../Shared/Navigation/Navigation';
+import AppointmentBanner from '../AppointmentBanner/AppointmentBanner';
+import Services from '../Services/Services';
 
 const Home = () => {
     return (
         <div>
             <Navigation></Navigation>
+            <Services></Services>
+            <AppointmentBanner></AppointmentBanner>
         </div>
     );
 };
