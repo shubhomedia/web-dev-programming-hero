aab71c89f3f351e18132a4c148a6f3e058bf14bc
MD.Owes Quruny Shubho <shubhomedia@gmail.com>
2022-11-12 14:04:07 +0600

    Update Appointment Banner  Component

---
+++ Milestone12/doctors-portal-client/src/Pages/Home/AppointmentBanner/AppointmentBanner.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AppointmentBanner = () => {
+    return (
+        <div>
+            <h2>This is Appointment</h2>
+        </div>
+    );
+};
+
+export default AppointmentBanner;
