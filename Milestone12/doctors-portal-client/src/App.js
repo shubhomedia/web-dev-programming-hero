fb7ef811ea013e43601ec86fca063848f037f265
MD.Owes Quruny Shubho <shubhomedia@gmail.com>
2022-11-12 14:02:50 +0600

    Update Apps Component

--- Milestone12/doctors-portal-client/src/App.js
+++ Milestone12/doctors-portal-client/src/App.js
@@ -1,11 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
 import Home from './Pages/Home/Home/Home';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import NotFound from './Pages/NotFound/NotFound';
 
 function App() {
   return (
     <div className="App">
-      <Home></Home>
+      <Router>
+        <Switch>
+          <Route exact path='/'>
+            <Home></Home>
+          </Route>
+          <Route path='/home'>
+            <Home></Home>
+          </Route>
+          <Route path='*'>
+            <NotFound></NotFound>
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
