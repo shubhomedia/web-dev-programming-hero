7b06aea01103494b898e83bd962e8dfceef6a88a
MD.Owes Quruny Shubho <shubhomedia@gmail.com>
2022-11-15 17:47:35 +0600

    Update Login File

---
+++ Milestone12/doctors-portal-client/src/Pages/Login/Login/Login.js
@@ -0,0 +1,71 @@
+import { Alert, Button, CircularProgress, Container, Grid, TextField, Typography } from '@mui/material';
+import React, { useState } from 'react';
+import { NavLink, useHistory, useLocation } from 'react-router-dom';
+import useAuth from '../../../Hooks/useAuth';
+import login from '../../../images/login.png'
+
+const Login = () => {
+
+    const [loginData, setLoginData] = useState({});
+    const { user, loginUser, isLoading, authError } = useAuth();
+
+    const location = useLocation();
+    const history = useHistory();
+
+    const handleOnChange = e => {
+        const field = e.target.name;
+        const value = e.target.value;
+        const newLoginData = { ...loginData };
+        newLoginData[field] = value;
+        setLoginData(newLoginData);
+    }
+
+    const handleLoginSubmit = e => {
+        loginUser(loginData.email, loginData.password, location, history);
+        e.preventDefault();
+    }
+
+    return (
+        <Container>
+            <Grid container spacing={2}>
+                <Grid sx={{ mt: 15 }} item xs={12} md={6}>
+                    <Typography variant="body1">Login</Typography>
+                    <form onSubmit={handleLoginSubmit}>
+                        <TextField
+                            sx={{ width: '75%', m: 1 }}
+                            id="standard-basic"
+                            label="Your Email"
+                            name="email"
+                            onChange={handleOnChange}
+                            variant="standard" />
+                        <TextField
+                            sx={{ width: '75%', m: 1 }}
+                            id="standard-basic"
+                            label="Your Password"
+                            name="password"
+                            onChange={handleOnChange}
+                            type="password"
+                            variant="standard" />
+                        <NavLink style={{ textDecoration: 'none' }} to='/register'>
+                            <Button variant="text">New User? Please Register</Button>
+                        </NavLink>
+                        <Button
+                            sx={{ width: '75%', m: 1 }}
+                            type='submit'
+                            variant='contained'
+                        >Login</Button>
+                        {isLoading && <CircularProgress></CircularProgress>}
+                        {user?.email && <Alert severity="success">This is a success alert — check it out!</Alert>}
+                        {authError && <Alert severity="error">This is a error alert — check it out!{authError}</Alert>
+                        }
+                    </form>
+                </Grid>
+                <Grid item xs={12} md={6}>
+                    <img style={{ width: '100%' }} src={login} alt="" srcset="" />
+                </Grid>
+            </Grid>
+        </Container >
+    );
+};
+
+export default Login;
