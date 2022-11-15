89a96519d1da7f928bbd95dd598e88e4cd59de72
MD.Owes Quruny Shubho <shubhomedia@gmail.com>
2022-11-15 17:46:31 +0600

    Update App File

--- Milestone12/doctors-portal-client/src/App.js
+++ Milestone12/doctors-portal-client/src/App.js
@@ -4,26 +4,38 @@ import Home from './Pages/Home/Home/Home';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import NotFound from './Pages/NotFound/NotFound';
 import Appointment from './Pages/Appointment/Appointment/Appointment';
+import Login from './Pages/Login/Login/Login';
+import Register from './Pages/Login/Register/Register';
+import AuthProvider from './Context/AuthProvider';
+import PrivateRoute from './Pages/Login/PrivateRoute/PrivateRoute';
 
 function App() {
   return (
     <div className="App">
-      <Router>
-        <Switch>
-          <Route exact path='/'>
-            <Home></Home>
-          </Route>
-          <Route path='/home'>
-            <Home></Home>
-          </Route>
-          <Route path='/appointment'>
-            <Appointment></Appointment>
-          </Route>
-          <Route path='*'>
-            <NotFound></NotFound>
-          </Route>
-        </Switch>
-      </Router>
+      <AuthProvider>
+        <Router>
+          <Switch>
+            <Route exact path='/'>
+              <Home></Home>
+            </Route>
+            <Route path='/home'>
+              <Home></Home>
+            </Route>
+            <Route path='/login'>
+              <Login></Login>
+            </Route>
+            <Route path='/register'>
+              <Register></Register>
+            </Route>
+            <PrivateRoute path='/appointment'>
+              <Appointment></Appointment>
+            </PrivateRoute>
+            <Route path='*'>
+              <NotFound></NotFound>
+            </Route>
+          </Switch>
+        </Router>
+      </AuthProvider>
     </div>
   );
 }
