74a440796013bca774c2f89c5ebf431d83ea4ec6
MD.Owes Quruny Shubho <shubhomedia@gmail.com>
2022-11-16 09:50:27 +0600

    Update Server File

--- Milestone12/doctors-portal-server/index.js
+++ Milestone12/doctors-portal-server/index.js
@@ -19,7 +19,28 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 async function run() {
     try {
         await client.connect();
-        console.log('Database Connent Succeessfully');
+        const database = client.db('doctors_portal');
+        const appointmentsCollection = database.collection('appointments');
+
+        app.get('/appointments', async (req, res) => {
+            const email = req.query.email;
+            const date = new Date(req.query.date).toLocaleDateString();
+
+            const query = { email: email, date: date }
+
+            const cursor = appointmentsCollection.find(query);
+            const appointments = await cursor.toArray();
+            res.json(appointments);
+        })
+
+
+        app.post('/appointments', async (req, res) => {
+            const appointment = req.body;
+            const result = await appointmentsCollection.insertOne(appointment);
+            console.log(result);
+            res.json(result)
+        });
+
     }
     finally {
         // await client.close();
@@ -35,3 +56,15 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
     console.log('Server Running At Port:', port);
 })
+
+
+
+
+
+// app.get('/users')
+// app.post('/users')
+// app.get('/users/:id')
+// app.put('/users/:id')
+// app.delete('/users/:id')
+// // Users : Get
+// // Users : Post
