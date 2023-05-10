544bb01bf40b8804c52a08d49f18aba0c5a889af
MD.Owes Quruny Shubho <shubhomedia@gmail.com>
2023-05-10 12:00:24 +0600

    Update Some Code
    Update and Practice components and probs

--- Milestone8/my-react-app/src/App.js
+++ Milestone8/my-react-app/src/App.js
@@ -5,7 +5,28 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h1>React JS API Work</h1>
+        <Person></Person>
+        <Friend phone="01700000000" address="Dhaka"></Friend>
       </header>
+    </div>
+  );
+}
+
+//Create Person Function Component
+function Person() {
+  return (
+    <div className="person">
+      <h3>This is Person Function</h3>
+    </div>
+  );
+}
+
+//create friend component
+function Friend(props) {
+  return (
+    <div className="friend">
+      <h3>Phone: {props.phone}</h3>
+      <h3>Address: {props.address}</h3>
     </div>
   );
 }
