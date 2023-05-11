f2cdac225440a12fe93bc25648d2daedb6fbce37
MD.Owes Quruny Shubho <shubhomedia@gmail.com>
2023-05-11 07:38:35 +0600

    new data added, map and show

--- Milestone8/my-react-app/src/App.js
+++ Milestone8/my-react-app/src/App.js
@@ -1,26 +1,71 @@
 import './App.css';
 
+const data = [{
+  "name": "Hannie Aspall",
+  "job": "Structural Engineer",
+  "salary": 19933
+}, {
+  "name": "Leone Miller",
+  "job": "Professor",
+  "salary": 13635
+}, {
+  "name": "Billie Proudman",
+  "job": "Accountant I",
+  "salary": 10997
+}, {
+  "name": "Abbie Goldingay",
+  "job": "Systems Administrator I",
+  "salary": 14164
+}, {
+  "name": "Sheilah Champley",
+  "job": "Occupational Therapist",
+  "salary": 17002
+}, {
+  "name": "Dolph Peetermann",
+  "job": "Marketing Assistant",
+  "salary": 15371
+}, {
+  "name": "Brandice Boxhall",
+  "job": "Senior Developer",
+  "salary": 16678
+}, {
+  "name": "Agace Mourant",
+  "job": "Help Desk Operator",
+  "salary": 12555
+}, {
+  "name": "Fawn Chick",
+  "job": "Technical Writer",
+  "salary": 15770
+}, {
+  "name": "Augusta Dounbare",
+  "job": "Systems Administrator I",
+  "salary": 15619
+}]
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <h1>React JS API Work</h1>
-        <Person></Person>
-        <Friend phone="01700000000" address="Dhaka"></Friend>
-        <Friend></Friend>
+        <h2> Total API Data: {data.length}</h2>
+        {
+          data.map(person => <Person name={person.name} job={person.job} salary={person.salary}></Person>)
+        }
       </header>
     </div>
   );
 }
 
-// //Create Person Function Component
-// function Person() {
-//   return (
-//     <div className="person">
-//       <h3>This is Person Function</h3>
-//     </div>
-//   );
-// }
+//Create Person Function Component
+function Person(props) {
+  return (
+    <div className="person">
+      <h3>Name : {props.name}</h3>
+      <h4>Job : {props.job}</h4>
+      <h5>Salary : {props.salary}</h5>
+    </div>
+  );
+}
 
 // //create friend component
 // function Friend(props) {
