3eccdcb620e1c5bb2d4f3b06315c015d88513640
MD.Owes Quruny Shubho <shubhomedia@gmail.com>
2023-05-19 13:58:50 +0600

    New Component Added

--- Milestone8/my-react-app/src/Components/CountryNameAndFlag/CountryNameAndFlag.js
+++ Milestone8/my-react-app/src/Components/CountryNameAndFlag/CountryNameAndFlag.js
@@ -1,13 +1,31 @@
-import React from 'react';
-
+import React, { useEffect, useState } from 'react';
+import './CountryNameAndFlag.css';
 const CountryNameAndFlag = () => {
+    const [country, setCountry] = useState([]);
+
+    useEffect(() => {
+        fetch('https://restcountries.com/v3.1/all')
+            .then(res => res.json())
+            .then(data => setCountry(data));
+    }, [])
+
     return (
         <div>
-            <h1>Country Name and Flag</h1>
+            <h3>Total Country : {country.length}</h3>
+            {
+                country.map(country => <Country name={country.name.common} flag={country.flags.png} ></Country>)
+            }
         </div>
     );
 };
 
-
+function Country(props) {
+    return (
+        <div className="country">
+            <h5>Country Name : {props.name}</h5>
+            <img src={props.flag} alt="" />
+        </div>
+    );
+}
 
 export default CountryNameAndFlag;
